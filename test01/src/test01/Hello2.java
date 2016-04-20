20f6d9d0522ce7847ee7ce75dce99479154c99a2
tmdguq2002 <suhy2002@naver.com>
2016-04-20 15:14:38 +0900

    .

--- test01/src/test01/Hello2.java
+++ test01/src/test01/Hello2.java
@@ -1,9 +1,9 @@
 package test01;
 
-public class Hello {
+public class Hello2 {
   public static void main(String[] args) {
     System.out.println("Hellod");
-    System.out.println("Hellod");
-    System.out.println("Hellod");
+    System.out.println("okok");
+    System.out.println("nono");
   }
 }
