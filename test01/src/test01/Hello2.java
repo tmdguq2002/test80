d1eb850875fe3473f7396c9a867134988ada1006
tmdguq2002 <suhy2002@naver.com>
2016-04-20 15:24:11 +0900

    .

--- test01/src/test01/Hello2.java
+++
@@ -1,9 +0,0 @@
-package test01;
-
-public class Hello2 {
-  public static void main(String[] args) {
-    System.out.println("Hellod");
-    System.out.println("okok");
-    System.out.println("nono");
-  }
-}
