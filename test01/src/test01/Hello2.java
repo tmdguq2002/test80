3d3607b1045e68300ce0fec11e698d5f655caf5b
tmdguq2002 <suhy2002@naver.com>
2016-04-20 15:03:27 +0900

    '.'

---
+++ test01/src/test01/Hello2.java
@@ -0,0 +1,9 @@
+package test01;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hellod");
+    System.out.println("Hellod");
+    System.out.println("Hellod");
+  }
+}
